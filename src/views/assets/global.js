97a687fc0367a5bc334a5ffb489c20fe3cb89e22
崮生 <admin@shenzilong.cn>
2020-11-24 22:50:55 +0800

    ✨feat(#3): 使用vditor进行代码块等的渲染

--- src/views/assets/global.js
+++ src/views/assets/global.js
@@ -16,4 +16,31 @@ function scrollIntoView(url) {
     }
 }
 window.addEventListener('hashchange', hashAndUpdate);
-window.addEventListener("load", () => scrollIntoView(location.href));
+window.addEventListener("load", () => {
+    // 当前链接可能指向了某一个块，尝试跳过去
+    scrollIntoView(location.href);
+
+    /** 用来获取 vditor 实例的工具元素 */
+    const d = document.createElement("div");
+    const id = "test__" + Date.now();
+    d.setAttribute("id", id);
+    d.style.display = "none";
+    document.body.appendChild(d);
+
+    const vditor = new Vditor(id).vditor;
+    const previewElement = document.querySelector('.markdown-body');
+    Vditor.setContentTheme(vditor.options.preview.theme.current, vditor.options.preview.theme.path);
+    Vditor.codeRender(previewElement);
+    Vditor.highlightRender(JSON.stringify(vditor.options.preview.hljs), previewElement, vditor.options.cdn);
+    Vditor.mathRender(previewElement, {
+        cdn: vditor.options.cdn,
+        math: JSON.stringify(vditor.options.preview.math)
+    });
+    Vditor.mermaidRender(previewElement, vditor.options.cdn);
+    Vditor.flowchartRender(previewElement, vditor.options.cdn);
+    Vditor.graphvizRender(previewElement, vditor.options.cdn);
+    Vditor.chartRender(previewElement, vditor.options.cdn);
+    Vditor.mindmapRender(previewElement, vditor.options.cdn);
+    Vditor.abcRender(previewElement, vditor.options.cdn);
+    Vditor.mediaRender(previewElement);
+});
