ed9cb681dd3839f56582e411d68397bb61a8d2b8
崮生 <admin@shenzilong.cn>
2020-12-23 16:08:00 +0800

    🦄refactor(font-end-lib): 修改dev与build的行为

--- font-end-lib/dev.mjs
+++ font-end-lib/dev.mjs
@@ -4,3 +4,12 @@ import fs from "fs";
 import fse from "fs-extra";
 
 fse.emptyDirSync('../docs/assets/font-end-lib/');
+
+const headHtmlPath = "../src/views/head.html";
+const headHtml = fs.readFileSync(headHtmlPath).toString();
+const assetsCode = `<!-- font-end-lib 生成的资源 star --><script type="module" src="http://localhost:3000/vite/client"></script>
+<script type="module" src="http://localhost:3000/src/main.js"></script><!-- font-end-lib 生成的资源 end -->`;
+
+const newHeadHtml = headHtml
+    .replace(/<!-- font-end-lib 生成的资源 star -->([\s\S]*)<!-- font-end-lib 生成的资源 end -->/, assetsCode);
+fs.writeFileSync(headHtmlPath, newHeadHtml);
