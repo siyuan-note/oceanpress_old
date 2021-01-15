098d2fc1d141a175a33f309175b36269baf433bb
崮生 <admin@shenzilong.cn>
2021-01-15 21:08:33 +0800

    test

--- build.bat
+++ build.bat
@@ -1,3 +1,21 @@
+SET GO111MODULE=on
+SET GOPROXY=https://goproxy.io
+
+SET CGO_ENABLED=1
+SET GOOS=linux
+SET GOARCH=amd64
+go build -x -v -o linux_md2website ./src
+
+SET CGO_ENABLED=1
+SET GOOS=windows
+SET GOARCH=amd64
+go build -x -v -o windows_md2website.exe ./src
+
+
+# https://kaixuan.im/2019/12/07/macos-golang-shi-yong-sqlite3zai-m-jiao-cha-windows-liunuxbian-yi-wen-ti/
+
+
+
 SET CGO_ENABLED=1
 SET GOOS=darwin
 SET GOARCH=amd64
@@ -6,12 +24,12 @@ go build -x -v -ldflags "-s -w" -o mac_md2website ./src
 SET CGO_ENABLED=1
 SET GOOS=linux
 SET GOARCH=amd64
-SET CC=x86_64-linux-musl-gcc
-SET CXX=aarch64-linux-gnu-g++
-go build -ldflags "-linkmode external -extldflags -static" -x -v -o linux_md2website ./src
+go build -x -v -o linux_md2website ./src
 
 SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=amd64
 go build -x -v -o windows_md2website.exe ./src
 # https://kaixuan.im/2019/12/07/macos-golang-shi-yong-sqlite3zai-m-jiao-cha-windows-liunuxbian-yi-wen-ti/
+
+xgo github.com/2234839/md2website/src
