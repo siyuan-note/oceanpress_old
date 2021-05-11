a3de214ccb59963d90c3c9b8bf1dead4a5025943
崮生 <admin@shenzilong.cn>
2021-05-11 15:31:00 +0800

    🦄 refactor(font-end): 调整样式

--- font-end-lib/src/components/block-ref.svelte
+++ font-end-lib/src/components/block-ref.svelte
@@ -1,27 +1,28 @@
+<svelte:options tag="md2website-block-ref" />
+
 <script lang="ts" context="module">
-    import { md2website } from "../lib/md2website.global";
-    //@ts-expect-error
-    export const gotoClick = (...arg) => md2website.gotoClick(...arg);
+  import { md2website } from "../lib/md2website.global";
+  //@ts-expect-error
+  export const gotoClick = (...arg) => md2website.gotoClick(...arg);
 </script>
 
 <script lang="ts">
-    /** ═════════🏳‍🌈 块引用 => 超链接 🏳‍🌈═════════  */
-    export let src: string;
-    export let title: string;
+  /** ═════════🏳‍🌈 块引用 => 超链接 🏳‍🌈═════════  */
+  export let src: string;
+  export let title: string;
 </script>
 
-<style>
-    [data-block-type="NodeBlockRefText"] {
-        color: var(--b3-vditor-mark-block-color);
-    }
-</style>
-
-<svelte:options tag="md2website-block-ref" />
-
 <a
-    class="c-block-ref"
-    data-block-type="NodeBlockRefText"
-    href={src}
-    on:click={gotoClick}>
-    {title}
+  class="c-block-ref"
+  data-block-type="NodeBlockRefText"
+  href={src}
+  on:click={gotoClick}
+>
+  {title}
 </a>
+
+<style>
+  [data-block-type="NodeBlockRefText"] {
+    color: var(--b3-protyle-inline-blockref-color);
+  }
+</style>
